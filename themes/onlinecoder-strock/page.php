ba06e2de698e3f7991de7879174372574620bd8d
shakibdshy <shakibdshy@gmail.com>
2019-08-26 00:48:27 +0600

    page.php update

--- themes/onlinecoder-strock/page.php
+++ themes/onlinecoder-strock/page.php
@@ -12,29 +12,58 @@
  * @package Strock
  */
 
-get_header();
+get_header(); 
+while ( have_posts() ) : the_post();
+
+	if(get_post_meta( get_the_ID(), 'my_post_options', true )){
+		$page_meta = get_post_meta( get_the_ID(), 'my_post_options', true );
+	}else {
+		$page_meta = array();
+	}
+
+	if(array_key_exists('enable_title', $page_meta)){
+		$enable_title = $page_meta['enable_title'];
+	}else {
+		$enable_title = true;
+	}
+
+	if(array_key_exists('custom_title', $page_meta)){
+		$custom_title = $page_meta['custom_title'];
+	}else {
+		$custom_title = '';
+	}
 ?>
+	<?php if($enable_title == true) : ?>
+	<div class="stock-breadcrumb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+				<h1 class="entry-title">
+					<?php if (!empty($custom_title)) { echo $custom_title; } else { the_title(); } ?>
+				</h1>
+				<?php if(function_exists('bnc_display')) bnc_display(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php endif;?>
 
 	<div class="stock-internal-area">
 		<div class="container">
 			<div class="row">
 				<div class="col-xl-12">
-				<?php
-				while ( have_posts() ) :
-					the_post();
-
-					get_template_part( 'template-parts/content', 'page' );
+				
+				<?php get_template_part( 'template-parts/content', 'page' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
 					endif;
-
-				endwhile; // End of the loop.
+				 // End of the loop.
 				?>
 				</div>
 			</div>
 		</div>
 	</div>
-<?php
+<?php endwhile;
 get_footer();
--- themes/onlinecoder-strock/template-parts/content-page.php
+++ themes/onlinecoder-strock/template-parts/content-page.php
@@ -6,42 +6,10 @@
  *
  * @package Strock
  */
-	if(get_post_meta( get_the_ID(), 'my_post_options', true )){
-		$page_meta = get_post_meta( get_the_ID(), 'my_post_options', true );
-	}else {
-		$page_meta = array();
-	}
 	
-	if(array_key_exists('enable_title', $page_meta)){
-		$enable_title = $page_meta['enable_title'];
-	}else {
-		$enable_title = true;
-	}
-
-	if(array_key_exists('custom_title', $page_meta)){
-		$custom_title = $page_meta['custom_title'];
-	}else {
-		$custom_title = '';
-	}
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-
-	<?php if($enable_title == true) : ?>
-	<header class="entry-header">
-		<h1 class="entry-title">
-			<?php if (!empty($custom_title)) {
-				echo $custom_title;
-			} else {
-				the_title();
-			}
-			?>
-		</h1>
-		<?php if(function_exists('bnc_display')) bnc_display(); ?>
-	</header><!-- .entry-header -->
-	<?php endif;?>
-
 
 	<?php onlinecoder_strock_post_thumbnail(); ?>
 
