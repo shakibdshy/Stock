2f61eebd1d57546fe5e1f139263094583b732ae5
shakibdshy <shakibdshy@gmail.com>
2019-08-26 02:24:04 +0600

    Tile shotcode & addons update

--- plugins/stock-toolkit/theme-shortcodes/tile-gallery-shortcode.php
+++ plugins/stock-toolkit/theme-shortcodes/tile-gallery-shortcode.php
@@ -5,6 +5,14 @@ function stock_tile_gallery_shortcode($atts, $content = null){
       'images'             => '',
       'height'             => 310,
       'custom_design'      => '',
+      'size'               => 'large',
+      'type'               => 1,
+      'top'                => '',
+      'bottom'             => '',
+      'left'               => '',
+      'right'              => '',
+      'center'             => '',
+
   ), $atts) );
 
   $image_ids = explode(',', $images);
@@ -12,13 +20,25 @@ function stock_tile_gallery_shortcode($atts, $content = null){
   $image_no = 0;
   $custom_design = vc_shortcode_custom_css_class( $custom_design, ' ' );
 
+  if ($type == 1) {
+      $position = $center;
+   } elseif($type == 2) {
+      $position = $top;
+   } elseif($type == 3) {
+      $position = $bottom;
+   } elseif($type == 4) {
+      $position = $left;
+   } else {
+      $position = $right;
+   }
 
   if (!empty($images)) {
    $stock_tile_gallery_markup .= '
-   <div class="stock-tile-gallery stock-tile-gallery-image-'.$image_no.'">';
+   <div class="'.$custom_design.' stock-tile-gallery stock-tile-gallery-image-'.$image_count.'">';
       foreach ($image_ids as $image) {
-         $image_array = wp_get_attachment_image_src($image, 'lage');
-         $stock_tile_gallery_markup .= '<div style="background-image: url('.$image_array[0].')" class="tile-gallery-block tile-gallery-block-'.$image_no.'"></div>';
+         $image_array = wp_get_attachment_image_src($image, $size);
+         $image_no++;
+         $stock_tile_gallery_markup .= '<div style="background-image: url('.$image_array[0].'); height:'.$height.'px; background-position: '.$position.'; background-size: cover;" class="tile-gallery-block tile-gallery-block-'.$image_no.'"></div>';
       }
 
       $stock_tile_gallery_markup .= '
--- plugins/stock-toolkit/vc-addons/vc-tile-gallery.php
+++ plugins/stock-toolkit/vc-addons/vc-tile-gallery.php
@@ -19,6 +19,70 @@ vc_map(
             "description" => esc_html__( "Type Image height in number", "stock" )
          ),
          array(
+            "type" => "dropdown",
+            "heading" => esc_html__( "Background Position", "stock" ),
+            "param_name" => "type",
+            "std" => esc_html__( "1", "stock" ),
+            "value" => array(
+               'Center' => 1, 
+               'Top'    => 2, 
+               'Bottom' => 3, 
+               'Left'   => 4, 
+               'Right'  => 5, 
+            ),
+            "description" => esc_html__( "", "stock" )
+         ),
+         array(
+            "type" => "dropdown",
+            "heading" => esc_html__( "Center", "stock" ),
+            "param_name" => "center",
+            "description" => esc_html__( "", "stock" ),
+            'dependency'   => array( 
+               'element'   => 'type', 
+               'value'     => array("1"),
+            )
+         ),
+         array(
+            "type" => "dropdown",
+            "heading" => esc_html__( "Top", "stock" ),
+            "param_name" => "top",
+            "description" => esc_html__( "", "stock" ),
+            'dependency'   => array( 
+               'element'   => 'type', 
+               'value'     => array("2"),
+            )
+         ),
+         array(
+            "type" => "dropdown",
+            "heading" => esc_html__( "Bottom", "stock" ),
+            "param_name" => "bottom",
+            "description" => esc_html__( "", "stock" ),
+            'dependency'   => array( 
+               'element'   => 'type', 
+               'value'     => array("3"),
+            )
+         ),
+         array(
+            "type" => "dropdown",
+            "heading" => esc_html__( "Left", "stock" ),
+            "param_name" => "left",
+            "description" => esc_html__( "", "stock" ),
+            'dependency'   => array( 
+               'element'   => 'type', 
+               'value'     => array("4"),
+            )
+         ),
+         array(
+            "type" => "dropdown",
+            "heading" => esc_html__( "Right", "stock" ),
+            "param_name" => "right",
+            "description" => esc_html__( "", "stock" ),
+            'dependency'   => array( 
+               'element'   => 'type', 
+               'value'     => array("5"),
+            )
+         ),
+         array(
 				'type' => 'css_editor',
 				'heading' => esc_html__( 'Css', 'stock' ),
 				'param_name' => 'custom_design',
