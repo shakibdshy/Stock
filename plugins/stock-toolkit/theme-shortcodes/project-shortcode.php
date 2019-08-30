9b94a360e1d2f46212ddb0ddba5f54150e58d865
shakibdshy <shakibdshy@gmail.com>
2019-08-30 22:06:00 +0600

    Update Project Shortcode Style-2

--- plugins/stock-toolkit/theme-shortcodes/project-shortcode.php
+++ plugins/stock-toolkit/theme-shortcodes/project-shortcode.php
@@ -2,7 +2,7 @@
 
 function stock_project_shortcode($atts, $content = null){
    extract( shortcode_atts( array(
-      'title'              => '',
+      'theme'              => '1',
   ), $atts) );
 
   $project_categories = get_terms('project_cat');
@@ -13,7 +13,7 @@ function stock_project_shortcode($atts, $content = null){
 
   <script>
      jQuery(document).ready(function($){
-        $(".stock-project-shorting li").click(function(){
+        $(".stock-project-s-active li").click(function(){
             $(".stock-project-shorting li").removeClass("active");
             $(this).addClass("active");
 
@@ -32,9 +32,19 @@ function stock_project_shortcode($atts, $content = null){
 
 
 
-   <div class="row">
-      <div class="col-md-3">
-         <ul class="stock-project-shorting">
+   <div class="row">';
+
+   if ($theme == '1') {
+      $stock_project_markup .= '
+      <div class="col-md-3">';
+      $project_list_class = 'stock-project-shorting';
+   }else {
+      $stock_project_markup .= '
+      <div class="col-md-12">';
+      $project_list_class = 'stock-project-shorting-2';
+   }
+         $stock_project_markup .= '
+         <ul class="stock-project-s-active '.$project_list_class.'">
             <li class="active" data-filter="*">All Works</li>';
 
             if( !empty($project_categories) && ! is_wp_error($project_categories)){
@@ -44,9 +54,22 @@ function stock_project_shortcode($atts, $content = null){
                }
             }
             $stock_project_markup .= '
-         </ul>
-      </div>
-      <div class="col-md-9">
+         </ul>';
+
+      $stock_project_markup .= '
+      </div>';
+
+
+      if ($theme == '1') {
+         $project_column_width = 'col-md-9';
+         $project_inner_column_width = 'col-md-4';
+      } else {
+         $project_column_width = 'col-md-12';
+         $project_inner_column_width = 'col-md-3';
+      }
+      
+      $stock_project_markup .= '
+      <div class="'.$project_column_width.'">
          <div class="row project-list-'.$dynamic_number.'">';
 
          $project_array = new WP_Query(
@@ -74,7 +97,7 @@ function stock_project_shortcode($atts, $content = null){
 
 
          $stock_project_markup .= '
-            <div class="col-md-4 '.$project_assigned_cat.'">
+            <div class="'.$project_inner_column_width.' '.$project_assigned_cat.'">
                <a href="'.get_permalink().'" class="single-work-box">
                   <div class="work-box-bg" style="background-image:url('.get_the_post_thumbnail_url(get_the_ID(), 'large').');">                  
                      <i class="fas fa-link"></i>
