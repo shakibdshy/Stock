3e866d8645ef26013a50017cc60d6cf485af532f
shakibdshy <shakibdshy@gmail.com>
2019-09-01 13:10:18 +0600

    Update index.php

--- themes/onlinecoder-strock/index.php
+++ themes/onlinecoder-strock/index.php
@@ -15,45 +15,54 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="stock-breadcrumb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<h1>Blog</h1>
+				</div>
+			</div>
+		</div>
+	</div>
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+	<div class="stock-internal-area section-enable-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
 				<?php
-			endif;
+				if ( have_posts() ) :
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					if ( is_home() && ! is_front_page() ) :
+						?>
+						<header>
+							<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+						</header>
+						<?php
+					endif;
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
 
-			endwhile;
+						
+						get_template_part( 'template-parts/content', get_post_type() );
 
-			the_posts_navigation();
+					endwhile;
 
-		else :
+					the_posts_navigation();
 
-			get_template_part( 'template-parts/content', 'none' );
+				else :
 
-		endif;
-		?>
+					get_template_part( 'template-parts/content', 'none' );
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				endif;
+				?>
+				</div>
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
 
 <?php
-get_sidebar();
+
 get_footer();
