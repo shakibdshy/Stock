0285b65739bf0b0f4ba629e133a56411425a214c
shakibdshy <shakibdshy@gmail.com>
2019-09-01 14:27:59 +0600

    Search.php page update

--- themes/onlinecoder-strock/search.php
+++ themes/onlinecoder-strock/search.php
@@ -9,47 +9,52 @@
 
 get_header();
 ?>
+	<div <?php if(has_post_thumbnail()) : ?>style="background-image: url(<?php the_post_thumbnail_url('large')?>)"<?php endif; ?> class="stock-breadcrumb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<h1 class="page-title">
+						<?php
+						/* translators: %s: search query. */
+						printf( esc_html__( 'Search Results for: %s', 'onlinecoder-strock' ), '<span>' . get_search_query() . '</span>' );
+						?>
+					</h1>
+				<?php if(function_exists('bcn_display')) bcn_display(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div class="stock-internal-area section-enable-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+				<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
 					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'onlinecoder-strock' ), '<span>' . get_search_query() . '</span>' );
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						
+						get_template_part( 'template-parts/content', 'search' );
+
+					endwhile;
+
+					the_posts_navigation();
+
+					else :
+
+					get_template_part( 'template-parts/content', 'none' );
+
+					endif;
 					?>
-				</h1>
-			</header><!-- .page-header -->
+				</div>
+				<?php get_sidebar();?>
+			</div>
+		</div>
+	</div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
