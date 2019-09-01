c5227919ff8ebe3487155a3befbe13a9201e1eef
shakibdshy <shakibdshy@gmail.com>
2019-09-01 14:18:44 +0600

    archive.php update

--- themes/onlinecoder-strock/archive.php
+++ themes/onlinecoder-strock/archive.php
@@ -10,44 +10,49 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<div <?php if(has_post_thumbnail()) : ?>style="background-image: url(<?php the_post_thumbnail_url('large')?>)"<?php endif; ?> class="stock-breadcrumb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+				<h1>
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+				</h1>
+				<?php if(function_exists('bcn_display')) bcn_display(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 
-		<?php if ( have_posts() ) : ?>
+	<div class="stock-internal-area section-enable-padding">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+						get_template_part( 'template-parts/content', get_post_type() );
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+					endwhile;
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+					the_posts_navigation();
 
-			endwhile;
+					else :
 
-			the_posts_navigation();
+					get_template_part( 'template-parts/content', 'none' );
 
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					endif;
+					?>
+				</div>
+				<?php get_sidebar();?>
+			</div>
+		</div>
+	</div>
 
 <?php
-get_sidebar();
 get_footer();
